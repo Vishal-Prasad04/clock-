b7ae69df533e2a824c0f28c4b40fe1479079b841
Vishal <vgour1240@gmail.com>
2023-03-09 14:40:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+},1000);
